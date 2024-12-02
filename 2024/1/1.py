15dd31641a3f5176824b95ca509725c3fb32b835
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-12-02 11:52:33 +0000

    Day 1

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,7 +1,7 @@
 lists = [[], []]
 with open('input.txt', 'r') as f:
     for line in f.readlines():
-        numbers =[int(w) for w in line.strip().split()]
+        numbers = [int(w) for w in line.strip().split()]
         for i in range(len(numbers)):
             lists[i].append(numbers[i])
 for i in range(len(lists)):
@@ -10,3 +10,13 @@ distance = 0
 for i in range(len(lists[0])):
     distance += abs(lists[0][i] - lists[1][i])
 print(distance)
+
+from collections import Counter
+
+c = Counter(lists[1])
+similarity = 0
+for n in lists[0]:
+    if n in c:
+        similarity += n * c[n]
+
+print(similarity)
